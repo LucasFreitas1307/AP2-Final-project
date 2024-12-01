45e8ec934e428fb4ffb89c548ad3190de3472cc1
Lucas D Freitas <167094976+LucasFreitas1307@users.noreply.github.com>
2024-12-01 20:25:12 -0300

    Update Criação de tarefas.c

--- Criação de tarefas.c
+++ Criação de tarefas.c
@@ -1,15 +1,16 @@
 #ifndef CRIARTAREFAS_H_INCLUDED
 #define CRIARTAREFAS_H_INCLUDED
+#define MAX_TAREFA 256
+
 void criarTarefa() {
     FILE *arquivo = fopen("tarefas.txt", "a");
-    if (!arquivo) {
+    if (arquivo == NULL) {
         printf("Erro ao abrir o arquivo!\n");
-        return;
+        return 0;
     }
 
     char tarefa[MAX_TAREFA];
-    printf("Digite a descrição da tarefa (max %d caracteres): ", MAX_TAREFA - 1);
-    getchar(); // Limpar o buffer do teclado
+    printf("Digite a descrição da tarefa (max %d caracteres): ", MAX_TAREFA );
     fgets(tarefa, MAX_TAREFA, stdin);
     tarefa[strcspn(tarefa, "\n")] = '\0'; // Remover o '\n' do final
 
@@ -17,7 +18,6 @@ void criarTarefa() {
     fclose(arquivo);
     printf("Tarefa salva com sucesso!\n");
 }
-
 
 
 #endif // CRIARTAREFAS_H_INCLUDED
